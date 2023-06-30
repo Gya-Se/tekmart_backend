d5754a16b81edecc7d25c515e772ab0f9c221ab0
unknown <sethgyan587@gmail.com>
2023-06-30 01:21:53 +0000

    added some changes

--- controller/vendor.controller.js
+++ controller/vendor.controller.js
@@ -138,35 +138,30 @@ const getVendor = asyncHandler(async (req, res) => {
 //Vendor login
 const vendorLogin = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
-    try {
-        const vendor = await Vendor.findOne({ email });
+    const vendor = await Vendor.findOne({ email });
 
-        if (vendor.role !== "vendor") throw new Error("Not Authorised");
+    if (vendor.role !== "vendor") throw new Error("Not Authorised");
 
-        if (vendor.isBlocked === true) throw new Error("Your account is blocked. Contact our customer service for support on how to recover your account");
+    if (vendor.isBlocked === true) throw new Error("Your account is blocked. Contact our customer service for support on how to recover your account");
 
-        if (vendor && (await vendor.isPasswordMatched(password))) {
-            const refreshToken = generateRefreshToken(vendor._id);
-            await Vendor.findByIdAndUpdate(vendor._id, { refreshToken: refreshToken },
-                { new: true }
-            );
-            res.cookie("refreshToken", refreshToken, {
-                httpOnly: true,
-                maxAge: 72 * 60 * 60 * 1000,
-                secure: false,
-            });
-            res.status(200).json({
-                _id: vendor._id,
-                name: vendor.name,
-                email: vendor.email,
-                token: generateToken(vendor._id)
-            });
-        } else {
-            throw new Error("Invalid Credentials");
-        }
-    } catch (error) {
-        console.error(error);
-        throw new Error(error);
+    if (vendor && (await vendor.isPasswordMatched(password))) {
+        const refreshToken = generateRefreshToken(vendor._id);
+        await Vendor.findByIdAndUpdate(vendor._id, { refreshToken: refreshToken },
+            { new: true }
+        );
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            maxAge: 72 * 60 * 60 * 1000,
+            secure: false,
+        });
+        res.status(200).json({
+            _id: vendor._id,
+            name: vendor.name,
+            email: vendor.email,
+            token: generateToken(vendor._id)
+        });
+    } else {
+        throw new Error("Invalid Credentials");
     }
 });
 
